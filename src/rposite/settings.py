b009cfab0ec67e4ef0bc541ec98390603763117c
Jim Martens <github@2martens.de>
2016-08-01 16:35:00 +0200

    [Project] Supporters must be verified now
    In addition the rpocore models are now grouped under
    a legible header 'Campaign' in the admin area.
    
    Signed-off-by: Jim Martens <github@2martens.de>

--- src/rposite/settings.py
+++ src/rposite/settings.py
@@ -257,6 +257,15 @@ INSTALLED_APPS = (
 
 AUTH_PROFILE_MODULE = "rpocore.Supporter"
 
+ADMIN_MENU_ORDER = (
+    ('Content', ('pages.Page', 'blog.BlogPost', 'generic.ThreadedComment', ('Media Library', 'media-library'))),
+    ('Site', ('sites.Site', 'redirects.Redirect', 'conf.Setting')),
+    ('Users', ('auth.User', 'auth.Group')),
+    ('Campaign', ('rpocore.SupportGroup',))
+)
+
+ACCOUNTS_VERIFICATION_REQUIRED = True
+
 # List of middleware classes to use. Order is important; in the request phase,
 # these middleware classes will be applied in the order given, and in the
 # response phase the middleware will be applied in reverse order.
