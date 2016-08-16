b52e6258cdbcb802a8e24e81a235d2b7edb95811
Jim Martens <github@2martens.de>
2016-08-16 17:41:07 +0200

    [Core] Implemented process progress bar
    Signed-off-by: Jim Martens <github@2martens.de>

--- src/rpocore/page_processors.py
+++ src/rpocore/page_processors.py
@@ -1,6 +1,7 @@
+from datetime import date
 from mezzanine.pages.page_processors import processor_for
 
-from rpocore.models import SupporterPage, Supporter, SupportGroup
+from rpocore.models import SupporterPage, Supporter, SupportGroup, HomepagePage, Phase, Process
 
 
 @processor_for(SupporterPage)
@@ -36,3 +37,36 @@ def support_statistics(request, page):
         )
 
     return {'uhh_count': uhh_count, 'other_count': other_count, 'all_count': all_count, 'data': data_by_group}
+
+
+@processor_for(HomepagePage)
+def progress_data(request, page):
+    phases_all = Phase.objects.all()
+    phases_used = phases_all.filter(process=page.homepagepage.process)
+    phases_successful = phases_used.filter(start_date__lt=date.today()).filter(end_date__lt=date.today())
+    phase_count = phases_used.count()
+    phase_successful_count = phases_successful.count()
+    width_remaining_progress = 1 if phase_successful_count + 1 > phase_count \
+        else (phase_successful_count + 1) / phase_count
+    phase_data = []
+    iterator = 1
+
+    for phase in phases_used:
+        division = 1 if iterator == phase_count else iterator / phase_count
+        current_percentage = "{:.0%}".format(division) if division != 1 else "1em"
+        data = {
+            'percentage': current_percentage,
+            'phase': phase
+        }
+        phase_data.append(data)
+        iterator += 1
+
+    return {
+        'phases': phase_data,
+        'process': page.homepagepage.process,
+        'amountOfPhases': phase_count,
+        'amountSuccessfulPhases': phase_successful_count,
+        'activePhaseNumber': phase_successful_count + 1,
+        'widthSuccessfulProgress': "{:.0%}".format(phase_successful_count / phase_count),
+        'widthRemainingProgress': "{:.0%}".format(width_remaining_progress)
+    }
